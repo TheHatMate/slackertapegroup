b8297ce8d7e1a1a214039541a54beed087228460
TheHatMate <bruno@allfour.ca>
2024-02-07 21:04:40 -0700

    Fixed mobile interface somwhat

--- pageJS.js
+++ pageJS.js
@@ -8,6 +8,6 @@ function getRandomSpinnerGif() {
     
     let rndNum = Math.floor(Math.random() * 66);
 
-    document.getElementsByClassName("spinner")[0].innerHTML = '<img src="https://slackertapegroup.com/img/gifs/' + rndNum + '.gif" height="125"/>';
-    document.getElementsByClassName("spinner")[1].innerHTML = '<img src="https://slackertapegroup.com/img/gifs/' + rndNum + '.gif" height="125"/>';
+    document.getElementsByClassName("spinner")[0].innerHTML = '<img class="spingif" src="https://slackertapegroup.com/img/gifs/' + rndNum + '.gif">';
+    document.getElementsByClassName("spinner")[1].innerHTML = '<img class="spingif" src="https://slackertapegroup.com/img/gifs/' + rndNum + '.gif">';
 }
