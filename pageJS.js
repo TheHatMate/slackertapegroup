b722b06dd4291b45a21400cacc1ffa33e6447d55
TheHatMate <bruno@allfour.ca>
2024-09-17 11:44:30 -0600

    announced low earth orbit ep, removed a gif, added news entry

--- pageJS.js
+++ pageJS.js
@@ -6,7 +6,7 @@ window.onload = function () {
 
 function getRandomSpinnerGif() {
     
-    let rndNum = Math.floor(Math.random() * 84);
+    let rndNum = Math.floor(Math.random() * 83);
 
     document.getElementsByClassName("spinner")[0].innerHTML = '<img class="spingif" src="https://slackertapegroup.com/img/gifs/' + rndNum + '.gif">';
     document.getElementsByClassName("spinner")[1].innerHTML = '<img class="spingif" src="https://slackertapegroup.com/img/gifs/' + rndNum + '.gif">';
